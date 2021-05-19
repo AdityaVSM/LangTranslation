c692809077bdbc6e59819ab4cc156237fb984496
AdityaVSM <adityavsm55@gmail.com>
2021-05-19 14:07:41 +0530

    added visual elements

--- app/src/main/java/com/example/langtranslation/MainActivity.java
+++ app/src/main/java/com/example/langtranslation/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.langtranslation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputText;
+    private TextView outputText;
+    private Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputText = findViewById(R.id.editTextTextInput);
+        outputText = findViewById(R.id.outputTextView);
+        goButton = findViewById(R.id.goButton);
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "go button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
